73f429394a9e95393c75dc5a30e845024a2772f5
Adithya Shivashankar <adithyamp44@gmail.com>
2026-08-03 15:19:30 +0530

    updated backfill

--- backfill_prices.py
+++ backfill_prices.py
@@ -7,7 +7,27 @@ nvda = yf.Ticker("NVDA")
 df = nvda.history(start="2026-04-13", end="2026-07-20").reset_index()
 
 if df.empty:
-	print("Data retrieval failed.")
+	print("🔴 Data retrieval failed. 🔴")
 	exit()
+
+df['date'] = df['Data'].dt.strftime('%Y-%m-%d')
+df['ticker'] = 'NVDA'
+df['open'] = df['Open'].round(2)
+df['high'] = df['High'].round(2)
+df['low'] = df['Low'].round(2)
+df['close'] = df['Close'].round(2)
+df['volume'] = df['Volume'].astype(int)
+
+df_final = df[['ticker', 'date', 'open', 'high', 'low', 'close','volume']]
+
+client = bigquery.Client()
+table_id = "stockmaxx.stock_warehouse.market_prices"
+
+job_config = bigquery.LoadJobConfig(write_disposition = bigquery.writeDisposition.WRITE_APPEND)
+
+job = client.load_table_from_datafeam(df_final, table_id, job_config)
+
+print("🟢 Successfully backfilled market price gap in BQ! 🟢")
+
 
 
